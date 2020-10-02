f84ac55878fcdda576ac339a5248fc094904580d
cgobet <cedric.gobet@epfl.ch>
2020-10-02 13:14:15 +0200

    update heatmap plotting function

--- R/function_plot_models_rhythm.R
+++ R/function_plot_models_rhythm.R
@@ -1,16 +1,18 @@
 #' Visualization of dryseq results
 #'
 #' This function allows to plot the results of dryseq.
-#' @param DF results from dryseq
+#' @param dryList from dryseq output
 #' @param file_path_name folder to store output
 #' @export pdf that summarize the results of dryseq
 #' @examples
 #' XYZn
-plot_models_rhythm = function(DF,file_path_name,time,group, period=24){
+plot_models_rhythm = function(dryList,file_path_name){
 
-  x=DF
+  t=dryList$time
+  group=dryList$group
+  x=dryList$results
 
-  pdf(file =paste(file_path_name,'.pdf',sep=''))
+  pdf(file =paste(file_path_name,'summary_heatmap_models.pdf',sep=''))
 
   nb = table(x[,'choosen_model'])
   nb = nb[order(-nb)]
@@ -29,10 +31,10 @@ plot_models_rhythm = function(DF,file_path_name,time,group, period=24){
         x_s = x_s[order(x_s[,pos_phase[min(which(sum_phase !=0))]]),]
       }
 
-      x_s=as.matrix(x_s[,1:length(time)])
+      x_s=as.matrix(x_s[,1:length(t)])
 
       ## Average replicates
-      com= paste(time,group)
+      com= paste(t,group)
       com.u=unique(com)
       t.u=sapply(strsplit(com.u," "),"[[",1)
       cond.u=sapply(strsplit(com.u," "),"[[",2)
@@ -45,23 +47,18 @@ plot_models_rhythm = function(DF,file_path_name,time,group, period=24){
       condi=sapply(strsplit(colnames(x_s.m)," "),"[[",2)
       ss=split(1:length(condi),condi)
 
-      for(k in ss){ x_s.m[,k]= sweep(x_s.m[,k],1,rowMeans(x_s.m[,k]),FUN="/") }
+      for(k in ss){ x_s.m[,k]= sweep(x_s.m[,k],1,rowMeans(x_s.m[,k]),FUN="-") }
       x_s.m=x_s.m[,match(com.u,colnames(x_s.m))]
 
 
-      gplots::heatmap.2(as.matrix(x_s.m),
-                Rowv = NA,
-                Colv = NA,
-                ylab = NA ,
-                dendrogram = "none",
-                labCol = paste('ZT',t.u, sep = "_"),
-                labRow = NA ,
-                scale = NULL,
-                trace='none',
-                colsep=which(diff(as.numeric(as.factor(cond.u)))!=0),
+      pheatmap(as.matrix(x_s.m),
+               cluster_cols = FALSE,
+               cluster_rows = FALSE,
+                show_rownames = F ,
+                scale = "row",
+                gaps_col=which(diff(as.numeric(as.factor(cond.u)))!=0),
                 main = paste("model",i," #Genes",nrow(x_s),sep =" "),
-                col = colorRampPalette(c('blue','black','yellow'))(1000),
-                key=F)
+                col = colorRampPalette(c('blue','yellow'))(1000))
 
 
       par(mfrow=c(1+round(length(pos_phase)/3),3))
@@ -75,7 +72,7 @@ plot_models_rhythm = function(DF,file_path_name,time,group, period=24){
       }
       bas = unique(sum_phase)
       la = match(bas,sum_phase)
-      if(length(unique(sum_phase)) > 1){
+      if(length(unique(sum_phase[sum_phase!=0])) > 1){
 
         pairs(x[,pos_phase[la]],cex=0.5)
 

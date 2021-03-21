60f297b40938f0ac7db3f1024b78353019adee0f
benjaminweger <64013365+benjaminweger@users.noreply.github.com>
2021-03-21 17:11:03 +1000

    Update misc_function.R

--- R/misc_function.R
+++ R/misc_function.R
@@ -365,14 +365,14 @@ dry_plot = function (dryList, gene)
 
   if(normal==FALSE) {m$value[which(m$value<0)] = 0}
 
-  gg1 = ggplot2::ggplot(d, aes(x=time, y=value, group=group, color=group)) +
+  gg1 = ggplot2::ggplot(d, ggplot2::aes(x=time, y=value, group=group, color=group)) +
     geom_errorbar(aes(ymin=value-se, ymax=value+se), width=.4) +
     geom_point(size=2, shape=19) +
     xlab("Time (h)") +
     ggtitle(ID) +
     scale_x_continuous(breaks=c(0,6,12,18,24,30)) +
     theme_bw(base_size = 10) +
-    theme(aspect.ratio = 1, panel.grid.minor=element_blank(), legend.position = "right") +
+    theme(aspect.ratio = 1, panel.grid.minor=ggplot2::element_blank(), legend.position = "right") +
     geom_line(aes(x=time, y=(value), group=group), data = m, position=position_dodge(width=0.5)) +
     facet_wrap(~group)
 
